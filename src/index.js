ccfbc16f2f6df4c2e35a69ea3bffb4ff18495345
Cameron Gray <this@cameronjgray.com>
2025-07-25 12:14:19 +0100

    chore(db): started db setup and added env vars

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = require('./routes');
 
+require('dotenv').config()
+
+const PORT = process.env.SERVER_PORT;
 const app = express();
 app.use(express.json());
 app.use(router);
-app.listen(8080, () => console.log('server running...'));
+app.listen(PORT, () => console.log(`server running on ${PORT}`));
 
